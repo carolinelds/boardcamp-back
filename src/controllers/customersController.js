bde627a46e517cdbc44dd6ab8f1f97bb6cf4e6c3
Caroline LS <carolinelds@gmail.com>
2022-05-31 03:44:44 -0300

    Configure endpoint to obtain a client with its id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,9 +1,9 @@
 import db from "./../db.js";
 
-export async function getCustomers(req,res){
+export async function getCustomers(req, res) {
     const filterString = req.query.cpf;
     try {
-        if (filterString){
+        if (filterString) {
             const query = `
                 SELECT customers.*  
                 FROM customers
@@ -22,28 +22,53 @@ export async function getCustomers(req,res){
 
             res.send(customers.rows);
         }
-    } catch(e){
+    } catch (e) {
         console.log(e);
         res.status(500).send("Ocorreu um erro ao obter os clientes!");
     }
 }
 
-export async function createCustomer(req,res){
-    const {name, phone, cpf, birthday} = req.body;
+export async function getCustomer(req, res) {
+    const { id } = req.params;
 
     try {
-        
+        const query = `
+            SELECT customers.*  
+            FROM customers
+            WHERE customers.id = $1
+        `;
+        const values = [id];
+        const customer = await db.query(query,values);
+
+        if (customer.rowCount === 0){
+            res.status(404).send("Não existe um cliente com o id procurado.");
+        }
+        res.send(customer.rows[0]);
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Ocorreu um erro ao obter os dados do cliente!");
+    }
+}
+
+export async function createCustomer(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+
         const query = `
             INSERT INTO customers (name, phone, cpf, birthday)
             VALUES ($1, $2, $3, $4)
         `;
         const values = [name, phone, cpf, birthday];
-        await db.query(query,values);
+        await db.query(query, values);
 
         res.sendStatus(201);
 
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.status(500).send("Ocorreu um erro ao cadastrar o cliente.");
     }
 }
+
+
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getCustomers, createCustomer } from "./../controllers/customersController.js";
+import { getCustomers, getCustomer, createCustomer } from "./../controllers/customersController.js";
 import { customersMiddleware } from "./../middlewares/customersMiddleware.js";
 
 const customersRouter = Router();
 
 customersRouter.get("/customers", getCustomers);
+customersRouter.get("/customers/:id", getCustomer);
 customersRouter.post("/customers", customersMiddleware, createCustomer);
 
 export default customersRouter;
