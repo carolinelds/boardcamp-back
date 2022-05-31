003d36b273265161afb7692edc42d8cbe3e5ca8f
Caroline LS <carolinelds@gmail.com>
2022-05-31 03:56:51 -0300

    Configure endpoint do edit customer's info

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -71,4 +71,29 @@ export async function createCustomer(req, res) {
     }
 }
 
+export async function updateCustomer(req,res){
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        const query = `
+            UPDATE customers
+            SET 
+                name = $1,
+                phone = $2,
+                cpf = $3,
+                birthday = $4
+            WHERE id = $5
+        `;
+        const values = [name, phone, cpf, birthday, id];
+        await db.query(query,values);
+        
+        res.sendStatus(200);
+        
+    } catch(e) {
+        console.log(e);
+        res.status(500).send("Ocorreu um erro ao editar os dados do cliente.");
+    }
+}
+
 
--- src/routes/customersRouter.js
+++ src/routes/customersRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getCustomers, getCustomer, createCustomer } from "./../controllers/customersController.js";
+import { getCustomers, getCustomer, createCustomer, updateCustomer } from "./../controllers/customersController.js";
 import { customersMiddleware } from "./../middlewares/customersMiddleware.js";
 
 const customersRouter = Router();
@@ -7,5 +7,6 @@ const customersRouter = Router();
 customersRouter.get("/customers", getCustomers);
 customersRouter.get("/customers/:id", getCustomer);
 customersRouter.post("/customers", customersMiddleware, createCustomer);
+customersRouter.put("/customers/:id", customersMiddleware, updateCustomer);
 
 export default customersRouter;
